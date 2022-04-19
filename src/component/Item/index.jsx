b5332b4d4b7a6436685e809ca2bd3370face5940
ZhaoDong <7942160+zhao_dong1181803971@user.noreply.gitee.com>
2022-04-19 22:15:45 +0800

    57

--- src/component/Item/index.jsx
+++ src/component/Item/index.jsx
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
 import './index.css'
 
-export default class Item
- extends Component {
+export default class Item extends Component {
+
   render() {
+    const {name,done} = this.props
     return (
       <li>
         <label>
-          <input type="checkbox"/>
-          <span>xxxxx</span>
+          <input type="checkbox" defaultChecked={done}/>
+          <span>{name}</span>
         </label>
-        <button className="btn btn-danger" style={{display:'none'}}>删除</button>
+        <button className="btn btn-danger" style={{ display: 'none' }}>删除</button>
       </li>
     )
   }
--- src/component/List/index.jsx
+++ src/component/List/index.jsx
@@ -4,9 +4,15 @@ import './index.css'
 
 export default class List extends Component {
   render() {
+    const { todos } = this.props
     return (
       <ul className="todo-main">
-        <Item />
+        {
+          todos.map(todo => {
+            return <Item key={todo.id} {...todo}/>
+          })
+        }
+
       </ul>
     )
   }
