f9d59d8ada656c789400f2178f3fa15d7411e80e
ZhaoDong <7942160+zhao_dong1181803971@user.noreply.gitee.com>
2022-04-20 22:16:48 +0800

    add style  p 59

--- src/component/Item/index.jsx
+++ src/component/Item/index.jsx
@@ -3,15 +3,25 @@ import './index.css'
 
 export default class Item extends Component {
 
+  state = {
+    mouse: false
+  }
+
+  handleMouse = (flag) => {
+    return () => {
+      this.setState({ mouse: flag })
+    }
+  }
   render() {
-    const {name,done} = this.props
+    const { name, done } = this.props
+    const { mouse } = this.state
     return (
-      <li>
+      <li onMouseEnter={this.handleMouse(true)} onMouseLeave={this.handleMouse(false)} style={{ backgroundColor: mouse ? '#ddd' : 'white' }}>
         <label>
-          <input type="checkbox" defaultChecked={done}/>
+          <input type="checkbox" defaultChecked={done} />
           <span>{name}</span>
         </label>
-        <button className="btn btn-danger" style={{ display: 'none' }}>删除</button>
+        <button className="btn btn-danger" style={{ display: mouse ? 'block' : 'none' }}>删除</button>
       </li>
     )
   }
